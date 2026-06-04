2a3669ea4eb6e5edeb943d0a9a7b78a892f559fd
Abhishek Sati <abhishek.sati@rolaface.com>
2026-06-04 18:01:04 +0530

    bypass if email trigger breaks

--- custom_hrms/api/expense/after_insert_hooks/after_insert.py
+++ custom_hrms/api/expense/after_insert_hooks/after_insert.py
@@ -4,17 +4,22 @@ from frappe.email.doctype.email_template.email_template import get_email_templat
 def trigger_email_notification(doc, method):
     if not doc.expense_approver:
         return
-
-    template = get_email_template(
-        "Expense Claim",
-        doc.as_dict()
-    )
-
-    frappe.sendmail(
-        recipients=[doc.expense_approver],
-        subject=template.get("subject"),
-        message=template.get("message"),
-        reference_doctype=doc.doctype,
-        reference_name=doc.name,
-        now=False,
-    )
+    try: 
+        template = get_email_template(
+            "Expense Claim",
+            doc.as_dict()
+        )
+        
+        frappe.sendmail(
+            recipients=[doc.expense_approver],
+            subject=template.get("subject"),
+            message=template.get("message"),
+            reference_doctype=doc.doctype,
+            reference_name=doc.name,
+            now=False,
+        )
+    except Exception as e:
+        frappe.log_error(
+            frappe.get_traceback(),
+            "Expense Claim Email Notification Error",
+        )
