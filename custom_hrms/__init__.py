dca35a96e47d00dd4a728356366e7772adede0bf
Akhilesh Rawat <akhileshrawat.asr@gmail.com>
2026-07-07 12:07:42 +0200

    feat(salary_slip): override email salry slip to send custom named salary slip attachment

--- custom_hrms/__init__.py
+++ custom_hrms/__init__.py
@@ -1,1 +1,5 @@
 __version__ = "0.0.1"
+import hrms.payroll.doctype.salary_slip.salary_slip as salary_slip_module
+from custom_hrms.overrides.email_salary_slip import patched_email_salary_slip
+
+salary_slip_module.SalarySlip.email_salary_slip = patched_email_salary_slip
