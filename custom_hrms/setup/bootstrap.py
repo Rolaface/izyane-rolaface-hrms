6c90a37f7629e84ed580631025195a091161e7ed
Akhilesh Rawat <akhileshrawat.asr@gmail.com>
2026-07-17 09:30:29 +0200

    feat(hrms_setup): uncomment runs

--- custom_hrms/setup/bootstrap.py
+++ custom_hrms/setup/bootstrap.py
@@ -1,5 +1,6 @@
+import frappe
 from .cleanup_hrms import cleanup_hrms
-# from .zambia_hrms import setup_zambia_hrms
+from .zambia_hrms_setup import setup_zambia_hrms
 
 
 def bootstrap():
@@ -7,10 +8,17 @@ def bootstrap():
     print("Starting HRMS Bootstrap")
     print("=" * 70)
 
+    print("Starting HRMS Cleanup")
+    print("=" * 70)
+
     cleanup_hrms()
 
-    # setup_zambia_hrms()
+    print("=" * 70)
+    print("Starting ZAMBIA HRMS Setup")
+    print("=" * 70)
 
-    # frappe.db.commit()
+    setup_zambia_hrms()
+
+    frappe.db.commit()
 
     print("\nHRMS Bootstrap Completed Successfully.")
